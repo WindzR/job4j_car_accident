bd5eb4a56948801625055f7f22cabd00bd4aa99e
WindzoR <windzr67@gmail.com>
2022-01-23 22:21:08 +0300

    Загрузка данных в вид

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -2,13 +2,32 @@ package ru.job4j.accident.control;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexControl {
 
-    @GetMapping("/")
+    @RequestMapping("/index")
     public String index(Model model) {
+        System.out.println("****Working controller IndexControl****");
+        List<String> list = new ArrayList<>();
+        list.add("Первый элемент списка");
+        list.add("Второй элемент списка");
+        list.add("Третий элемент списка");
+        model.addAttribute("itemsList", list);
         return "index";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmpDetails() {
+        return "ask-emp-details";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showDetails() {
+        return "show-emp-details";
     }
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,10 +1,29 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Accident</title>
 </head>
-<body>
-Hello : Accident
+
+<body class="container">
+<table class="table">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Описание</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <c:forEach items="${itemsList}" var="item" varStatus="count">
+        <tr>
+            <td><c:out value="${count.count}"/></td>
+            <td><c:out value="${item}"/></td>
+        </tr>
+    </c:forEach>
+
+    </tbody>
+</table>
 </body>
 </html>
