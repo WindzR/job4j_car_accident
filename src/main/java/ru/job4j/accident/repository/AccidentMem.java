093f84a2619ec27c1e9426b9112a9991a51d3a77
WindzoR <windzr67@gmail.com>
2022-01-26 22:54:16 +0300

    IndexControl. Таблица и вид.

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -10,32 +10,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Repository
 public class AccidentMem {
 
-    private final AtomicInteger count = new AtomicInteger(1);
+    private final AtomicInteger count = new AtomicInteger(3);
 
     private final Map<Integer, Accident> accidents = new HashMap<>();
 
     private final Map<Integer, AccidentType> types = new HashMap<>();
 
     public AccidentMem() {
-        types.put(1, AccidentType.of(1, "Две машины"));
-        types.put(2, AccidentType.of(2, "Машина и человек"));
-        types.put(3, AccidentType.of(3, "Машина и велосипед"));
-        types.put(4, AccidentType.of(4, "Одно транспортное средство"));
-    }
-
-    public static AccidentMem of() {
-        AccidentMem accidentMem = new AccidentMem();
         Accident collision = Accident.of(1, "Авария",
                 "статья 113, 189",
                 "Столкновение 2х легковых транспотных средств",
                 "Москва, Проспект Мира 112");
+        AccidentType col = AccidentType.of(1, "Две машины");
+        collision.setType(col);
         Accident overSpeed = Accident.of(2, "Превышение скоростного режима",
                 "статья 34",
                 "Превышение скоростного режима более чем на 20 км/ч",
                 "Москва, Ярославское шоссе, 13 км");
-        accidentMem.addAccident(collision);
-        accidentMem.addAccident(overSpeed);
-        return accidentMem;
+        AccidentType speed = AccidentType.of(4, "Одно транспортное средство");
+        overSpeed.setType(speed);
+        accidents.put(1, collision);
+        accidents.put(2, overSpeed);
+        types.put(1, AccidentType.of(1, "Две машины"));
+        types.put(2, AccidentType.of(2, "Машина и человек"));
+        types.put(3, AccidentType.of(3, "Машина и велосипед"));
+        types.put(4, AccidentType.of(4, "Одно транспортное средство"));
     }
 
     public void addAccident(Accident accident) {
